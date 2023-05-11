a16d6617f7cc71ba0e234b62aaebafa3d0eeed0f
RashadMusayev23 <rashadm@crocusoft.com>
2023-05-11 12:57:26 +0400

    Setup Smack Library Gradle Plugin

--- app/src/main/java/com/mazeppa/xmppchat/MainActivity.kt
+++ app/src/main/java/com/mazeppa/xmppchat/MainActivity.kt
@@ -2,11 +2,23 @@ package com.mazeppa.xmppchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var connectionConfiguration: XMPPTCPConnectionConfiguration? = null
+
+        try {
+            connectionConfiguration = XMPPTCPConnectionConfiguration.builder()
+                .setUsernameAndPassword("username", "password")
+                .setXmppDomain("server.im")
+                .setKeystoreType(null)
+                .build()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
